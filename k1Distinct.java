525c18c380d51884fed5b0586f1dbdb9d60b7a89
Jing Qin <jingqin@Jings-iMac.local>
2018-03-27 16:18:47 -0700

    find substrings of size k which has k-1 distinct characters

---
+++ k1Distinct.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.*;
+
+/*
+ * To execute Java, please define "static void main" on a class
+ * named Solution.
+ *
+ * If you need more classes, simply define them inline.
+ */
+
+class Solution {
+  public static List<String> find(String s, int k) {
+    
+    if (s == null || s.length() == 0 || k == 0) {
+      return new ArrayList<String>();
+    }
+    
+    
+    int[] hash = new int[26];
+    for (int i = 0; i < k - 1; i++) {
+      hash[s.charAt(i) - 'a']++;
+    }
+    
+    Set<String> set = new LinkedHashSet<>();
+    for (int i = k - 1; i < s.length(); i++) {
+      char c = s.charAt(i);
+      hash[c - 'a']++;
+      
+      if (valid(hash)) {
+        set.add(s.substring(i - k + 1, i + 1));
+      }
+      
+      hash[s.charAt(i - k + 1) - 'a']--;
+    }
+    
+    return new ArrayList<String>(set);
+  }
+  
+  private static boolean valid(int[] hash) {
+    
+    boolean flag = false;
+    for (int i = 0; i < hash.length; i++) {
+      if (hash[i] > 1) {
+        if (flag) {
+          return false;
+        }
+        flag = true;
+      }
+      
+    }
+    if (flag) 
+      return true;
+    
+    return false;
+  }
+        
+    
+  public static void main(String[] args) {
+    String s = "aabcddef";
+    List<String> ans = find(s, 4);
+    
+    for (String x : ans) {
+      System.out.println(x);
+    }
+  }
+}
+
